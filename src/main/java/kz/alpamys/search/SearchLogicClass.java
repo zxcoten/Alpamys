d95e6c05924398720383abf805c5a2156ed61498
unknown <Admin@KST-CPM-WS12.nis.edu.kz>
2024-02-20 17:21:45 +0600

    First Commit Alpamys bot

--- src/main/java/kz/alpamys/search/SearchLogicClass.java
+++ src/main/java/kz/alpamys/search/SearchLogicClass.java
@@ -7,8 +7,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 
 public class SearchLogicClass{
- String emailOfInput;
- String typeOfInput;
+
     public void doSmth(String value, String type){
         try (java.io.InputStream is = new java.net.URL("https://leakcheck.io/api?key=f1582185ff143821025c20f3b0a3a9fd0263d9d6&check=" + value + "&type=" + type).openStream()) {
             {
